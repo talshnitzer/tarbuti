b8f51a3379ed44d2b0b13fe72237f5bf59df0587
rachelri <rachel@owlytics.com>
2021-01-07 18:49:24 +0200

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
+//R.R: gere as well, why not use async await? also it's better practive to wrap this in an initialize function and call that function on your startup, this way you have more controll over the mongo connection
 mongoose.connect(process.env.MONGODB_URI, 
         { useNewUrlParser: true, useUnifiedTopology: true } ,
         (err, client) => {
             if (err) {
+                //R.R: not the best idea to print the MONGODB_URI it can contain sensative info like user password of your DB
                 return console.log('Unable to connect to MongoDB server', process.env.MONGODB_URI);
             }
         console.log('Connected to MongoDB server');
